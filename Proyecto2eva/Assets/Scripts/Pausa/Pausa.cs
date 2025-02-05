7b38265d337704ad9a08242d67b334421f37f519
AsierMUI <asieravino@gmail.com>
2025-02-05 09:53:52 +0100

    pausa

--- Proyecto2eva/Assets/Scripts/Pausa/Pausa.cs
+++ Proyecto2eva/Assets/Scripts/Pausa/Pausa.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Pausa : MonoBehaviour
 {
+
+    public GameObject menuPausa;
+
+    public bool pausa = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,28 @@ public class Pausa : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if(pausa == false)
+            {
+                menuPausa.SetActive(true);
+                pausa = true;
+
+                Time.timeScale = 0;
+            }
+            else if(pausa == true)
+            {
+                Reanudar();
+            }
+        }
     }
+
+
+    public void Reanudar()
+    {
+        menuPausa.SetActive(false);
+        pausa = false;
+        Time.timeScale = 1;
+    }
+
 }
