cdbe38c4e33790802d3faf6cb54cef7d6c7ed27f
AsierMUI <asieravino@gmail.com>
2025-02-11 17:15:00 +0100

    a

--- Proyecto2eva/Assets/Scripts/Inputs/Personaje/PlayerController.cs
+++ Proyecto2eva/Assets/Scripts/Inputs/Personaje/PlayerController.cs
@@ -9,17 +9,28 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private PlayerInput playerInput;
     private Vector2 movement;
+    Animator anim;
+    private Vector2 input;
+    private Vector2 lastMoveDirection;
+    private bool facingLeft = true;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if(input.x <0 && !facingLeft || input.x < 0 && facingLeft)
+                {
+            Flip();
+        }
+
+
     }
 
     #region Input Methods
@@ -29,4 +40,14 @@ public class PlayerController : MonoBehaviour
     }
 
     #endregion
+
+    
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        facingLeft = !facingLeft;
+    }
+
 }
