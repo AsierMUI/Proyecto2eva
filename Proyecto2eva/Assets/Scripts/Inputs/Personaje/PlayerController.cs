9be47b9aea638ed22fcd925f95fac9dba9579f81
AsierMUI <asieravino@gmail.com>
2025-02-12 17:50:03 +0100

    a

--- Proyecto2eva/Assets/Scripts/Inputs/Personaje/PlayerController.cs
+++ Proyecto2eva/Assets/Scripts/Inputs/Personaje/PlayerController.cs
@@ -14,17 +14,7 @@ public class PlayerController : MonoBehaviour
     private Vector2 movementDirection;
     private bool isAttacking = false;
 
-    public string walkUpAnim;
-    public string walkDownAnim;
-    public string walkLeftAnim;
-    public string walkRightAnim;
-    public string attackUpAnim;
-    public string attackDownAnim;
-    public string attackLeftAnim;
-    public string attackRightAnim;
-    public string idleAnim;
-
-
+  
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -34,7 +24,6 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        HandleInputs();
         MoveCharacter();
     }
 
@@ -50,112 +39,5 @@ public class PlayerController : MonoBehaviour
     }
     #endregion
 
-    private void HandleInputs()
-    {
-        if (isAttacking) return;
 
-        movementDirection = movement;
-
-        if (movementDirection.sqrMagnitude > 0)
-        {
-            animator.SetBool("IsMoving", true);
-
-            if (movementDirection.y > 0)
-            {
-                animator.SetBool(walkUpAnim, true);
-                animator.SetBool(walkDownAnim, false);
-                animator.SetBool(walkLeftAnim, false);
-                animator.SetBool(walkRightAnim, false);
-            }
-            else if (movementDirection.y < 0)
-            {
-                animator.SetBool(walkUpAnim, false);
-                animator.SetBool(walkDownAnim, true);
-                animator.SetBool(walkLeftAnim, false);
-                animator.SetBool(walkRightAnim, false);
-            }
-            else if (movementDirection.x < 0)
-            {
-                animator.SetBool(walkUpAnim, false);
-                animator.SetBool(walkDownAnim, false);
-                animator.SetBool(walkLeftAnim, true);
-                animator.SetBool(walkRightAnim, false);
-            }
-            else if (movementDirection.x > 0)
-            {
-                animator.SetBool(walkUpAnim, false);
-                animator.SetBool(walkDownAnim, false);
-                animator.SetBool(walkLeftAnim, false);
-                animator.SetBool(walkRightAnim, true);
-            }
-        }
-        else
-        {
-            animator.SetBool("IsMoving", false);
-            animator.SetBool(walkUpAnim, false);
-            animator.SetBool(walkDownAnim, false);
-            animator.SetBool(walkLeftAnim, false);
-            animator.SetBool(walkRightAnim, false);
-            animator.SetBool(idleAnim, true);
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            HandleAttack();
-        }
-    }
-
-    private void HandleAttack()
-    {
-        isAttacking = true;
-        animator.SetBool("IsAttacking", true);
-
-        if (movementDirection.y > 0)
-        {
-            animator.SetBool(attackUpAnim, true);
-            animator.SetBool(attackDownAnim, false);
-            animator.SetBool(attackLeftAnim, false);
-            animator.SetBool(attackRightAnim, false);
-
-        }
-        else if (movementDirection.y < 0)
-        {
-            animator.SetBool(attackUpAnim, false);
-            animator.SetBool(attackDownAnim, true);
-            animator.SetBool(attackLeftAnim, false);
-            animator.SetBool(attackRightAnim, false);
-        }
-        else if (movementDirection.x < 0)
-        {
-            animator.SetBool(attackUpAnim, false);
-            animator.SetBool(attackDownAnim, false);
-            animator.SetBool(attackLeftAnim, true);
-            animator.SetBool(attackRightAnim, false);
-        }
-        else if (movementDirection.x > 0)
-        {
-            animator.SetBool(attackUpAnim, false);
-            animator.SetBool(attackDownAnim, false);
-            animator.SetBool(attackLeftAnim, false);
-            animator.SetBool(attackRightAnim, true);
-        }
-        else
-        {
-            animator.SetBool("IsAttacking", false);
-            animator.SetBool(attackUpAnim, false);
-            animator.SetBool(attackDownAnim, false);
-            animator.SetBool(attackLeftAnim, false);
-            animator.SetBool(attackRightAnim, false);
-        }
-
-        StartCoroutine(AttackCooldown());
-    }
-
-    private System.Collections.IEnumerator AttackCooldown()
-    {
-        yield return new WaitForSeconds(5f);
-        animator.SetBool("IsAttacking", false);
-        isAttacking = false;
-    }
 }
