27e79cc6e7a3058052eea5238810ee006e03a030
AsierMUI <asieravino@gmail.com>
2025-02-12 11:54:55 +0100

    a

--- Proyecto2eva/Assets/Scripts/Inputs/Personaje/PlayerController.cs
+++ Proyecto2eva/Assets/Scripts/Inputs/Personaje/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,27 +10,33 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private PlayerInput playerInput;
     private Vector2 movement;
-    Animator anim;
+    Animator animator;
     private Vector2 input;
-    private Vector2 lastMoveDirection;
-    private bool facingLeft = true;
+    private Vector2 movementDirection;
+    private bool isAttacking = false;
+
+
+    public string walkUpAnim;
+    public string walkDownAnim;
+    public string walkLeftAnim;
+    public string walkRightAnim;
+    public string attackUpAnim;
+    public string attackDownAnim;
+    public string attackLeftAnim;
+    public string attackRightAnim;
+
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
-
     void Update()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        if(input.x <0 && !facingLeft || input.x < 0 && facingLeft)
-                {
-            Flip();
-        }
-
+        HandleInputs();
 
     }
 
@@ -41,13 +48,62 @@ public class PlayerController : MonoBehaviour
 
     #endregion
 
-    
-    void Flip()
+
+    private void HandleInputs()
     {
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
-        facingLeft = !facingLeft;
+        if (isAttacking) return;
+
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("WalkUp");
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetTrigger("WalkDown");
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetTrigger("WalkLeft");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetTrigger("WalkRight");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightAlt)) 
+        {
+            HandleAttack();
+
+        }
+    }
+    private void HandleAttack()
+    {
+        isAttacking = true;
+        if (movementDirection.y > 0)
+        {
+            animator.SetTrigger("AttackUp");
+        }
+        else if (movementDirection.y < 0)
+        {
+            animator.SetTrigger("AttackDown");
+        }
+        else if (movementDirection.x < 0)
+        {
+            animator.SetTrigger("AttackLeft");
+        }
+        else if (movementDirection.x > 0)
+        {
+            animator.SetTrigger("AttackRight");
+        }
+        StartCoroutine(AttackCooldown());
+    }
+    private System.Collections.IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(1f);
+        isAttacking = false;
     }
 
 }
