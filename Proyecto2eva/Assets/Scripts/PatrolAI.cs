c343df1785c180b59c91ef8917b930566b0d0664
AsierMUI <asieravino@gmail.com>
2025-02-13 17:43:32 +0100

    <

--- Proyecto2eva/Assets/Scripts/PatrolAI.cs
+++ Proyecto2eva/Assets/Scripts/PatrolAI.cs
@@ -1,23 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PatrolAI : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Transform[] waypoints;
-
-    private int currentWaypoint;
-
-    void Update()
+    [SerializeField] private float minDistance;
+    private int numeroAleatorio;
+    private SpriteRenderer spriteRenderer;
+    private void Start()
     {
-        if(transform.position != waypoints[currentWaypoint].position)
+        numeroAleatorio = Random.Range(0, waypoints.Length);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[numeroAleatorio].position, speed * Time.deltaTime);
+        
+        if (Vector2.Distance(transform.position, waypoints[numeroAleatorio].position) < minDistance)
         {
-            transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypoint].position, speed * Time.deltaTime);
+            numeroAleatorio = Random.Range(0,waypoints.Length);
+            FLip();
+        }
+    }
+    private void FLip()
+    {
+        if (transform.position.x < waypoints[numeroAleatorio].position.x)
+        {
+            spriteRenderer.flipX = false;
         }
         else
         {
-            currentWaypoint++;
+            spriteRenderer.flipX= true;
         }
     }
 }
