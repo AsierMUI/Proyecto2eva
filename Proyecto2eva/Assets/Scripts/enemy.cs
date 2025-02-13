771828b1b1180c273aafe1d9dd7743e0dfed6579
IzSaGu <izansanchezguillem@gmail.com>
2025-02-13 13:39:06 +0100

    sefir

---
+++ Proyecto2eva/Assets/Scripts/enemy.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemy : MonoBehaviour
+{
+    public Transform player;
+    public float detectionRadius = 5.0F;
+    public float speed = 2.0f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        if (distanceToPlayer < detectionRadius)
+        {
+            Vector2 direction = (player.position - transform.position)
+.normalized;
+            movement = new Vector2(direction.x, 0);
+        }
+        else
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            collision.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            SceneManager.LoadScene(6);
+            gameObject.SetActive(false);
+        }
+    }
+}
