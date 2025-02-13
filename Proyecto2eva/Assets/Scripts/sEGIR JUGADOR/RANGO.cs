42b475cbeee45945415edd580c39b253e7016bdf
IzSaGu <izansanchezguillem@gmail.com>
2025-02-13 12:09:02 +0100

    dad

--- Proyecto2eva/Assets/Scripts/sEGIR JUGADOR/RANGO.cs
+++ Proyecto2eva/Assets/Scripts/sEGIR JUGADOR/RANGO.cs
@@ -15,6 +15,9 @@ public class RANGO : MonoBehaviour
     public Vector3 puntoInicial;
 
     public bool mirandoDerecha;
+    public Rigidbody2D rb2D;
+    public Animator animator;
+
 
     public EstadosMovimiento estadoActual;
     public enum EstadosMovimiento
@@ -70,6 +73,9 @@ public class RANGO : MonoBehaviour
     
 
     transform.position = Vector2.MoveTowards(transform.position, transformJugador.position, velocidadMovimiento* Time.deltaTime);
+
+        GirarAObjetivo(transformJugador.position);
+
     if (Vector2.Distance(transform.position, puntoInicial) > distanciaMaxima ||
         Vector2.Distance(transform.position, transformJugador.position) > distanciaMaxima)
 
@@ -80,6 +86,9 @@ public class RANGO : MonoBehaviour
     private void Estadovolviendo()
     {
     transform.position = Vector2.MoveTowards(transform.position, puntoInicial, velocidadMovimiento * Time.deltaTime);
+
+        GirarAObjetivo(puntoInicial);
+
         if(Vector2.Distance(transform.position, puntoInicial)< 0.1f)
         {
             estadoActual = EstadosMovimiento.Esperando;
@@ -90,10 +99,18 @@ public class RANGO : MonoBehaviour
     {
         if (objetivo.x > transform.position.x && !mirandoDerecha)
         {
+            Girar();
         }
         else if (objetivo.x < transform.position.x && mirandoDerecha)
-        { 
+        {
+            Girar();
         }
+    }
+
+    private void Girar()
+    {
+        mirandoDerecha = !mirandoDerecha;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
     }
 
     private void OnDrawGizmos()
